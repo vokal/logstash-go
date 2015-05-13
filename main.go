e5405a92214a0aa51ef8e4195aab507b67bcb2c3
Scott Ferguson <scottwferg@gmail.com>
2015-05-13 09:15:28 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package logstash
+
+// https://github.com/bobrik/logstasher/blob/master/writer.go
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+type Writer struct {
+	w      io.Writer
+	fields map[string]interface{}
+}
+
+func NewWriter(w io.Writer, f map[string]interface{}) Writer {
+	return Writer{
+		w:      w,
+		fields: f,
+	}
+}
+
+func (w Writer) Write(p []byte) (n int, err error) {
+	event := map[string]interface{}{
+		"@timestamp": time.Now(),
+		"message":    string(p),
+	}
+
+	for k, v := range w.fields {
+		event[k] = v
+	}
+
+	return len(p), json.NewEncoder(w.w).Encode(e)
+}
